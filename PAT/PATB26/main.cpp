ed50e17125886f856e8442796ea6029f5b0e664c
xuyanbo03 <610958401@qq.com>
2018-01-11 13:44:53 +0800

    add

--- PAT/PATB26/main.cpp
+++ PAT/PATB26/main.cpp
@@ -1,6 +1,28 @@
 #include <iostream>
 using namespace std;
+#define CLKTCK 100
 
 int main(){
+    double c1,c2;
+    cin>>c1>>c2;
+    double time=((c2-c1)+50)/CLKTCK;
+    int hour=time/60/60;
+    int minute=(time-hour*60*60)/60;
+    int second=(time-hour*60*60-minute*60);
+    if(hour<10){
+        cout<<"0"<<hour<<":";
+    }else{
+        cout<<hour<<":";
+    }
+    if(minute<10){
+        cout<<"0"<<minute<<":";
+    }else{
+        cout<<minute<<":";
+    }
+    if(second<10){
+        cout<<"0"<<second;
+    }else{
+        cout<<second;
+    }
     return 0;
 }
