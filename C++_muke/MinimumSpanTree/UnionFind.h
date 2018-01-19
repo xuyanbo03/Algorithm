e604d3671fca18af06612d4aa39e067e32aedb81
xuyanbo03 <610958401@qq.com>
2018-01-20 02:39:30 +0800

    add

---
+++ C++_muke/MinimumSpanTree/UnionFind.h
@@ -0,0 +1,76 @@
+//
+// Created by Awebone on 2018/1/20.
+//
+
+#ifndef MINIMUMSPANTREE_UNIONFIND_H
+#define MINIMUMSPANTREE_UNIONFIND_H
+
+#include <iostream>
+#include <cassert>
+
+using namespace std;
+
+//路径压缩的优化
+class UnionFind {
+private:
+    int *parent;
+    int *rank;//rank[i]表示以i为根的集合所表示的树的层数
+    int count;
+
+public:
+    UnionFind(int count) {
+        parent = new int[count];
+        rank = new int[count];
+        this->count = count;
+        for (int i = 0; i < count; i++) {
+            parent[i] = i;
+            rank[i] = 1;
+        }
+    }
+
+    ~UnionFind() {
+        delete[] parent;
+        delete[] rank;
+    }
+
+    //查
+    int find(int p) {
+        assert(p >= 0 && p < count);
+        //路径压缩1
+//        while (p != parent[p]) {
+//            parent[p] = parent[parent[p]];
+//            p = parent[p];
+//        }
+//        return p;
+        //路径压缩2,rank=1
+        if (p != parent[p]) {
+            parent[p] = find(parent[p]);
+        }
+        return parent[p];
+    }
+
+    //连接问题：是否连接
+    bool isConnected(int p, int q) {
+        return find(p) == find(q);
+    }
+
+    //并,找到根节点,进行连接
+    void unionElements(int p, int q) {
+        int pRoot = find(p);
+        int qRoot = find(q);
+        if (pRoot == qRoot) {
+            return;
+        }
+        if (rank[pRoot] < rank[qRoot]) {
+            parent[pRoot] = qRoot;
+        } else if (rank[pRoot] > rank[qRoot]) {
+            parent[qRoot] = pRoot;
+        } else {
+            //rank[pRoot] == rank[qRoot]
+            parent[pRoot] = qRoot;
+            rank[qRoot] += 1;
+        }
+    }
+};
+
+#endif //MINIMUMSPANTREE_UNIONFIND_H
--- C++_muke/MinimumSpanTree/main.cpp
+++ C++_muke/MinimumSpanTree/main.cpp
@@ -7,6 +7,7 @@
 #include "ReadGraph.h"
 #include "LazyPrimMST.h"
 #include "PrimMST.h"
+#include "KruskalMST.h"
 
 using namespace std;
 
@@ -28,31 +29,42 @@ int main() {
 //    cout << endl;
 
 
-//    string filename = "testG1.txt";
-//    int V = 8;
-//
-//    SparseGraph<double> g = SparseGraph<double>(V, false);
-//    ReadGraph<SparseGraph<double>, double> readGraph(g, filename);
-//
-//    //Test Lazy Prim MST
-//    cout << "Test Lazy Prim MST:" << endl;
-//    LazyPrimMST<SparseGraph<double>, double> lazyPrimMST(g);
-//    vector<Edge<double>> mst = lazyPrimMST.mstEdges();
-//    for (int i = 0; i < mst.size(); i++) {
-//        cout << mst[i] << endl;
-//    }
-//    cout << "The MST weight is: " << lazyPrimMST.result() << endl;
-//    cout << endl;
-//
-//    // Test Prim MST
-//    cout << "Test Prim MST:" << endl;
-//    PrimMST<SparseGraph<double>, double> primMST(g);
-//    mst = primMST.mstEdges();
-//    for (int i = 0; i < mst.size(); i++) {
-//        cout << mst[i] << endl;
-//    }
-//    cout << "The MST weight is: " << primMST.result() << endl;
-//    cout << endl;
+    string filename = "testG1.txt";
+    int V = 8;
+
+    SparseGraph<double> g = SparseGraph<double>(V, false);
+    ReadGraph<SparseGraph<double>, double> readGraph(g, filename);
+
+    //Test Lazy Prim MST
+    //O(ElogE)
+    cout << "Test Lazy Prim MST:" << endl;
+    LazyPrimMST<SparseGraph<double>, double> lazyPrimMST(g);
+    vector<Edge<double>> mst = lazyPrimMST.mstEdges();
+    for (int i = 0; i < mst.size(); i++) {
+        cout << mst[i] << endl;
+    }
+    cout << "The MST weight is: " << lazyPrimMST.result() << endl;
+    cout << endl;
+
+    // Test Prim MST
+    //O(ElogV)
+    cout << "Test Prim MST:" << endl;
+    PrimMST<SparseGraph<double>, double> primMST(g);
+    mst = primMST.mstEdges();
+    for (int i = 0; i < mst.size(); i++) {
+        cout << mst[i] << endl;
+    }
+    cout << "The MST weight is: " << primMST.result() << endl;
+    cout << endl;
+
+    // Test Kruskal MST
+    //O(ElogE)
+    cout << "Test Kruskal MST:" << endl;
+    KruskalMST<SparseGraph<double>, double> kruskalMST(g);
+    mst = kruskalMST.mstEdges();
+    for (int i = 0; i < mst.size(); i++)
+        cout << mst[i] << endl;
+    cout << "The MST weight is: " << kruskalMST.result() << endl;
 
     return 0;
 }
